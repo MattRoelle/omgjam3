57797d12bee4ebac68dfbd4b5f3cbd2e6911465d
Matthew Roelle <mroelle@renovateamerica.com>
2018-02-11 15:19:30 -0800

    Version number

--- src/controllers/main-menu-controller.js
+++ src/controllers/main-menu-controller.js
@@ -2,6 +2,13 @@ class MainMenuController {
 	constructor(params) {
 		this.sprite = game.phaser.add.sprite(0, 0, "title");
 
+		this.versionText = game.phaser.add.text(10, 660, "v1.0", {
+			font: "20px slkscr",
+			fill: "#ffffff",
+			stroke: "#000000",
+			strokeThickeness: 4
+		});
+
 		this.hasExited = false;
 		this.onMainCb = () => {
 			if (this.hasExited) return;
@@ -20,5 +27,6 @@ class MainMenuController {
 	destroy() {
 		game.input.onMain.remove(this.onMainCb, this);
 		this.sprite.destroy();
+		this.versionText.destroy();
 	}
 }
